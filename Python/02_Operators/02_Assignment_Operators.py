20e3ce4429548002ab9d4fdfd3ed481b9208e929
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-12 22:53:07 +0530

    Added to operators

---
+++ Python/02_Operators/02_Assignment_Operators.py
@@ -0,0 +1,31 @@
+# Assignment Operators
+num = 10
+
+print("Value:", num)
+
+num += 5
+print("After += :", num)
+
+num -= 2
+print("After -= :", num)
+
+num *= 3
+print("After *= :", num)
+
+num /= 2
+print("After /= :", num)
+
+num = 10
+
+num //= 3
+print("After //= :", num)
+
+num = 10
+
+num %= 3
+print("After %= :", num)
+
+num = 2
+
+num **= 3
+print("After **= :", num)
