8ee90553a60f38b8059f621d629c35c69f92e2cc
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-12 22:58:17 +0530

    Added to operators

---
+++ Python/02_Operators/04_Logical_Operators.py
@@ -0,0 +1,10 @@
+# Logical Operators
+a = 10
+b = 5
+
+print("a =", a)
+print("b =", b)
+
+print("a > 5 and b > 2 :", a > 5 and b > 2)
+print("a > 5 or b > 10 :", a > 5 or b > 10)
+print("not(a > 5) :", not (a > 5))
