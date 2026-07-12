c293666161ac55b9a8a463e9b3a93fd82ffbf16d
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-12 22:59:58 +0530

    Added to operators

---
+++ Python/02_Operators/05_Bitwise_Operators.py
@@ -0,0 +1,13 @@
+# Bitwise Operators
+a = 10
+b = 4
+
+print("a =", a)
+print("b =", b)
+
+print("a & b :", a & b)
+print("a | b :", a | b)
+print("a ^ b :", a ^ b)
+print("~a :", ~a)
+print("a << 1 :", a << 1)
+print("a >> 1 :", a >> 1)
