c4230302a83a32903cf3ac1497d0468e3fff0d3d
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-12 22:56:52 +0530

    Added to operators

---
+++ Python/02_Operators/03_Comparison_Operators.py
@@ -0,0 +1,13 @@
+# Comparison Operators
+num1 = 10
+num2 = 5
+
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("num1 == num2 :", num1 == num2)
+print("num1 != num2 :", num1 != num2)
+print("num1 > num2 :", num1 > num2)
+print("num1 < num2 :", num1 < num2)
+print("num1 >= num2 :", num1 >= num2)
+print("num1 <= num2 :", num1 <= num2)
