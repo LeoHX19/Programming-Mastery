2aef7310501dd58e5dafc57e9396b3c22ab7f8b1
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-14 00:10:14 +0530

    Added to operator quiz

---
+++ Python/02_Operators/Operator_Quiz/03_Intermediate_Quiz.py
@@ -0,0 +1,12 @@
+# Intermediate Operator Quiz
+
+a = 12
+b = 5
+
+print("What is the output?\n")
+
+print("1.", a // b)
+print("2.", a % b)
+print("3.", a ** 2)
+print("4.", a > 10 and b < 10)
+print("5.", not (a == b))
---
+++ Python/02_Operators/Operator_Quiz/04_Advanced_Quiz.py
@@ -0,0 +1,12 @@
+# Advanced Operator Quiz
+
+a = 8
+b = 3
+
+print("What is the output?\n")
+
+print("1.", a + b * 2)
+print("2.", (a + b) * 2)
+print("3.", a & b)
+print("4.", a | b)
+print("5.", a << 1)
