1529f0d6cd51fb7c2bb2a385929240215b73b15b
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-14 00:34:03 +0530

    Added operator quiz

---
+++ Python/02_Operators/Operator_Quiz/05_Challenge_Quiz.py
@@ -0,0 +1,11 @@
+# Challenge Operator Quiz
+a = 15
+b = 4
+
+print("Predict the output before running!\n")
+
+print("1.", a % b + 2)
+print("2.", a > b and b < 5)
+print("3.", a == 15 or b == 10)
+print("4.", not (a < b))
+print("5.", (a + b) // 3)
