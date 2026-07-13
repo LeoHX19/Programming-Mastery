cee2e696b5d5732ae750719a28bacc70b4a046ec
LeoHX19 <mailtoharikeshhari@gmail.com>
2026-07-13 23:41:06 +0530

    Added to operators

---
+++ Python/02_Operators/08_Operator_Precedence.py
@@ -0,0 +1,10 @@
+# Operator Precedence
+result1 = 10 + 5 * 2
+result2 = (10 + 5) * 2
+result3 = 20 / 2 + 3
+result4 = 20 / (2 + 3)
+
+print("10 + 5 * 2 =", result1)
+print("(10 + 5) * 2 =", result2)
+print("20 / 2 + 3 =", result3)
+print("20 / (2 + 3) =", result4)
